ebca94dec3c0aa9dccc4b4c3f5c2ea2566ab3e95
Viktor Karasev <viktorkarasev27@gmail.com>
2022-12-04 08:37:14 +0300

    added configuration for server

--- SocketChat.Server/Program.fs
+++ SocketChat.Server/Program.fs
@@ -1,4 +1,36 @@
-﻿open SocketChat.Base.Server
+﻿open System
+open SocketChat.Base.Server
+
+
+let rec readAddress () =
+    Console.Write "Enter the address of the server: "
+    let s = Console.ReadLine ()
+    try
+        System.Net.IPAddress.Parse(s)
+    with
+    | :? FormatException ->
+        printfn "Invalid address. Please try again."
+        readAddress ()
+
+let rec readPort () =
+    Console.Write "Enter the port of the server: "
+    let s = Console.ReadLine ()
+    try
+        int s
+    with
+    | :? FormatException ->
+        printfn "Invalid port. Please try again."
+        readPort ()
+
+let rec initServer () =
+    let address = readAddress ()
+    let port = readPort ()
+    try
+        Server(address, port)
+    with
+    | :? System.Net.Sockets.SocketException ->
+        printfn "Could not create server. Please try again."
+        initServer ()
 
 
 [<EntryPoint>]
@@ -7,7 +39,9 @@ let main _ =
     SocketChat.Base.Logging.ConfigureLogLevel SocketChat.Base.Logging.Debug
     #endif
 
-    let address = System.Net.IPAddress.Parse("127.0.0.1")
-    let server = Server(address, 8080)
-    server.Run()
+    let server = initServer ()
+    try
+        server.Run()
+    with
+    | :? System.Net.Sockets.SocketException -> printfn "Server stopped."
     0
